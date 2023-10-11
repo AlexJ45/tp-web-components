4a062b6b8221a74ba95cd260ccb87a18fcb4f967
AlexJ <alexandre.j45000@gmail.com>
2023-10-11 09:21:40 +0200

    /

--- src/current-time.js
+++ src/current-time.js
@@ -1,8 +1,4 @@
 class CurrentTime extends HTMLElement {
-  constructor() {
-    super();
-    this.intervalId = null;
-  }
   static get observedAttributes() {
     return ["format"];
   }
@@ -15,6 +11,7 @@ class CurrentTime extends HTMLElement {
     }
   }
   connectedCallback() {
+    this.intervalId = null;
     this.render();
     this.intervalId = setInterval(() => {
       if (this.format == "utc") {
@@ -30,7 +27,7 @@ class CurrentTime extends HTMLElement {
     clearInterval(this.intervalId);
   }
   render() {
-    /*this.title = this.format === "utc" ? "Heure UTC : "Heure Locale" */
+    /*this.title = this.format === "utc" ? "Heure UTC ": "Heure Locale" */
     if (this.format == "utc") {
       this.$date = new Date().toUTCString();
       this.title = "Heure UTC";
@@ -45,5 +42,63 @@ class CurrentTime extends HTMLElement {
     this.$time = this.querySelector("time");
   }
 }
+class ScreenSize extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.unit = "px";
+    this.render();
+    this.resizeListener = this.renderSize().bind(this);
+    window.addEventListener("resize", this.resizeListener);
+    this.$btn = this.shadowRoot.querySelector("button");
 
+    if (this.$btn) {
+      this.$btn.addEventListener("click", () => {
+        this.unit = this.unit === "px" ? "rem" : "px";
+        this.render();
+      });
+    }
+  }
+  static get observedAttributes() {
+    return ["unit"];
+  }
+  attributeChangedCallback(attr, oldVal, newVal) {
+    if (attr === "unit" && oldVal !== newVal) {
+      this.unit = newVal;
+    }
+  }
+  renderSize() {
+    this.width =
+      this.unit === "px" ? window.innerWidth : window.innerWidth / 16;
+  }
+  render() {
+    this.btntext = this.unit === "px" ? "rem" : "px";
+
+    this.renderSize();
+    this.shadowRoot.innerHTML = `
+    <style>
+    :host{
+      color: rgb(234, 234, 234);
+        background-color: rgb(38, 38, 38);
+        border-radius: 20px;
+        position: fixed;
+        top: 0;
+        right: 0;
+        padding: 1rem;
+        margin: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;}
+        </style>
+    <p>${this.width} ${this.unit}</p>
+    <button>${this.btntext}</button>
+    `;
+  }
+
+  disconnectedCallback() {
+    this.resizeListener = null;
+  }
+}
 customElements.define("current-time", CurrentTime);
+customElements.define("screen-size", ScreenSize);
